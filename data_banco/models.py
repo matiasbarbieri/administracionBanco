a234ac06aa45dc856deefa149f9dff60259db03d
Matias Barbieri <matias.nbarbieri@gmail.com>
2023-06-29 20:06:02 -0300

    init commit

---
+++ data_banco/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    tipo_documento = models.CharField(max_length=20)
+    numero_documento = models.CharField(max_length=20)
+    cuit_cuil = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=100)
+    localidad = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Habitacion(models.Model):
+    habitacion_id = models.IntegerField(primary_key=True)
+    numero_habitacion = models.IntegerField()
+    capacidad = models.IntegerField()
+    numero_camas = models.IntegerField()
+    ubicacion = models.CharField(max_length=50)
+    detalle = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Habitación {self.numero_habitacion}"
+
+class Reserva(models.Model):
+    reserva_id = models.IntegerField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    habitacion = models.ForeignKey(Habitacion, on_delete=models.CASCADE)
+    fecha_entrada = models.DateField()
+    fecha_salida = models.DateField()
+    total_pagar = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Reserva #{self.reserva_id} - {self.cliente} - {self.habitacion}"
---
+++ front_banco/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FrontBancoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'front_banco'
